5f0c079e591e8ce135654434fa73e600674f3786
Niles Bontrager <artoftheniles@yahoo.com>
2024-12-22 15:14:28 -0500

    Schema and Seeds created

--- server/schema/schema.sql
+++ server/schema/schema.sql
@@ -6,25 +6,23 @@ CREATE DATABASE company_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    FOREIGN KEY department_id
-    REFERENCES department(id)
-    INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY role_id
-    REFERENCES rold(id)
-    INTEGER NOT NULL,
-    FOREIGN KEY manager_id
-    REFERENCES employee(id),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
